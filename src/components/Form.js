1115cc7d6020ff923fd986a57815c8a4e7f31de1
erinwoodard8 <erinwoodard8@gmail.com>
2023-05-09 16:58:29 -0400

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import "./Form.css";
+
+const Form = () => {
+    const myFunction = (event) => {
+        event.preventDefault();
+        console.log("poop");
+    }
+    return (
+      <div className="form-container">
+        <form>
+          <div className="form-field">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="age">Age:</label>
+            <input type="number" id="age" name="age" />
+          </div>
+          <button type="submit" onClick={myFunction}className="submit-button">Submit</button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Form;
